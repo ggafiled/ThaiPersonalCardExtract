6e6994f3bdcb5370e162b606bb72893124004017
ggafiled <gafewkik234@gmail.com>
2021-09-02 23:04:55 +0700

    added function extract front and back of cvid card

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 setuptools.setup(
     name='thai-personal-card-extract',
     include_package_data=True,
-    version='1.3.3',
+    version='1.3.4',
     install_requires=requirements,
     license='Apache License 2.0',
     description='Library for extract infomation from thai personal identity card',
